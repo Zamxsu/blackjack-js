a490e0c1c7c13c1ba57d60afab7c13af1f7aa13b
Martin! <martin.rubiano1234@gmail.com>
2023-05-12 13:19:29 -0500

    Fin temario creacion de funciones

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -53,23 +53,16 @@ pedirCarta();
 
 const valorCarta = ( carta ) => {
     const valor = carta.substring (0, carta.length - 1);
-    let puntos = 0;
-
-    if ( isNaN( valor ) ) {
-        
-        puntos = ( valor === 'A' ) ? 11 : 10;
-
-    } else {
-        
-        puntos = valor * 1
-    }
+     return ( isNaN( valor ) ) ?
+     ( valor === 'A') ? 11 : 10
+     : valor * 1;
 
 
-    console.log( puntos )
-}  
+}              
 
 
-valorCarta( '3D')
+ const valor = valorCarta( 'KD')
+ console.log({ valor })
 
 
 
