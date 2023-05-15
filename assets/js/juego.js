c53aa9e4a9346b45b54f6fc99dcf0db36565f83b
Martin! <martin.rubiano1234@gmail.com>
2023-05-15 11:57:36 -0500

    Creando nuevamente la deck

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -2,29 +2,29 @@
 
 let deck     = [];
 const tipos  = ['C','D','H','S']
-const letras = ['A','J','Q','K']
+const especiales = ['A','J','Q','K']
 
 
 // Esta funcion crea una nueva baraja
 
 const crearDeck = () => {
 
-    for( let i = 2; i <= 10; i++) {
-        for( let tipo of tipos){
+    for(let i = 2 ; i <= 10; i++ ){
+        for( tipo of tipos ){
             deck.push( i + tipo)
         }
     }
-     
-    for( let tipo of tipos) {
-        for( let esp of letras){
-            deck.push( esp + tipo)
+
+    for( let tipo of tipos){
+        for(let esp of especiales){
+            deck.push( tipo + esp)
         }
     }
 
-    // console.log( deck )
-    deck = _.shuffle( deck );
-
     console.log( deck )
+
+
+
 
 }
 
@@ -32,37 +32,37 @@ crearDeck();
 
 // Esta funcion me permitira tomar una carta
 
-const pedirCarta = () => {
+// const pedirCarta = () => {
 
-    if ( deck.length === 0){
-        throw 'No hay cartas en el deck!';
-    }
+//     if ( deck.length === 0){
+//         throw 'No hay cartas en el deck!';
+//     }
 
-    const carta = deck.pop();
+//     const carta = deck.pop();
     
-    console.log( deck );
-    console.log( carta );
-    return carta;
+//     console.log( deck );
+//     console.log( carta );
+//     return carta;
 
-}
-
-
-pedirCarta();
-
-// Esta funcion me dara el valor de la carta
-
-const valorCarta = ( carta ) => {
-    const valor = carta.substring (0, carta.length - 1);
-     return ( isNaN( valor ) ) ?
-     ( valor === 'A') ? 11 : 10
-     : valor * 1;
+// }
 
 
-}              
+// pedirCarta();
+
+// // Esta funcion me dara el valor de la carta
+
+// const valorCarta = ( carta ) => {
+//     const valor = carta.substring (0, carta.length - 1);
+//      return ( isNaN( valor ) ) ?
+//      ( valor === 'A') ? 11 : 10
+//      : valor * 1;
 
 
- const valor = valorCarta( pedirCarta() )
- console.log({ valor })
+// }              
+
+
+//  const valor = valorCarta( pedirCarta() )
+//  console.log({ valor })
 
 
 
--- assets/js/pracs-ciclo-for.js
+++ assets/js/pracs-ciclo-for.js
@@ -1,11 +1,22 @@
 
+console.warn('Practica Ciclo For')
+
+const juegos =  ['League of Legends', 'Call of Duty','Valorant', 'Warzone','Blackjack','Raft','Sea Of Thieves'];
+
+
+const misJuegosFavoritos = () => {
+
+    for( let juego of juegos){
+        console.log( juego )
+    }
 
 
 
+}
 
+misJuegosFavoritos();
 
-
-
+ 
 
 
 
