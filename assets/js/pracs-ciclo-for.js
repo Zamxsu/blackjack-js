58439fb14fa2adb2c1732621dc2128ba006add5f
Martin! <martin.rubiano1234@gmail.com>
2023-05-16 15:08:38 -0500

    haciendo practicas con el blackjack

--- assets/js/pracs-ciclo-for.js
+++ assets/js/pracs-ciclo-for.js
@@ -1,25 +1,59 @@
 
 console.warn('Practica Ciclo For')
 
-const juegos =  ['League of Legends', 'Call of Duty','Valorant', 'Warzone','Blackjack','Raft','Sea Of Thieves'];
+// Poner cada curso de un colegio en un ciclo for
+
+let baraja = [];
+const palo =  ['T', 'D', 'P','C']
+const gradosEspeciales = ['A', 'J', 'Q', 'K']
 
 
-const misJuegosFavoritos = () => {
+const creandoBaraja =  () => {
 
-    for( let juego of juegos){
-        console.log( juego )
+    for( let i = 2; i <= 10; i++){
+        for(  let pal of palo ){
+            baraja.push( i + pal)
+        }
     }
 
+    for( let esp of gradosEspeciales){
+        for( let pal of palo){
+            baraja.push( esp + pal )
+        }
+    }
+
+
+    console.log( baraja )
+
+}
+
+creandoBaraja();
+
+const sacarCartas = () => {
+
+   
+   
+   
+    const cartaEliminada = baraja.pop()
+    console.log( baraja )
+    console.log( cartaEliminada )
 
 
 }
 
-misJuegosFavoritos();
-
- 
+sacarCartas();
 
 
+const valorCarta = ( carta ) => {
+    
+    const valor = carta.substring( 0 , carta.length - 1);
+    return ( isNaN( valor ) ) ?
+            ( valor === 'A') ? 11 : 10
+            : valor * 1;
+}              
 
 
+ const valor = valorCarta( pedirCarta() )
+ console.log({ valor })
 
 
